28e5063a172b60b81355286e272fddc3c94d8f42
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-10-07 09:28:03 +0300

    Adds the .filter method

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+//function to return the matching list of drivers
+function findMatching(drivers, string) {
+    return drivers.filter(function(matchList) {
+        return matchList.toLowerCase() === string.toLowerCase();
+    })
+}
+
+//function to return all drivers whose names begin with the provided letters
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(firstLetters) {
+        return firstLetters.slice(0, string.length) === string;
+    })
+}
+//function to return each element whose name property matches the provided string argument
+function matchName(drivers, string) {
+    const driver = [
+        {
+        "hometown": "",
+        "name": ""
+    }
+];
+    return drivers.filter(function(driver){
+        return driver.name === string;
+    })
+}
